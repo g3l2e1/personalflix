b90e6855ef5b7ab2e249aa5bc6a73fefb542e042
Gleyson Barroso <g3l2e1@gmail.com>
2019-11-14 00:23:47 -0300

    projeto criado com teste de pagina inicial em helloworld

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    
+    <nav class="flex items-center justify-between flex-wrap p-6">
+      <div class="text-white mr-6">
+        <img id="pflix-logo-nav" src="./assets/personalflix-logo-branco.svg">
+      </div>
+      <div class="">
+        <div>
+          <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">
+            Entrar
+          </a>
+        </div>
+      </div>
+    </nav>
+
+    <router-view/>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+#pflix-logo-nav { height: 40px; }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="max-w-sm rounded overflow-hidden" id="initial-card">
+    <div class="px-6 py-4">
+      <h1>Personalflix</h1>
+      <p class="text-gray-300 text-base">
+        A Netflix-style cloud-hosted personal movie manager. Um gerenciador de filmes pessoais.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#initial-card { position: fixed; bottom: 1em; left: 0.5rem; }
+#initial-card h1 { font-size: 2rem;  color: dodgerblue; font-weight: bolder; }
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
